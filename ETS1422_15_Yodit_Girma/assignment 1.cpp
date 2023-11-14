caa6992cae3e556de0d4dba623f8d2c0f939c72e
yoditgirma <149189296+yoditgirma@users.noreply.github.com>
2023-11-14 21:19:06 +0300

    Update assignment 1.cpp

--- ETS1422_15_Yodit_Girma/assignment 1.cpp
+++ ETS1422_15_Yodit_Girma/assignment 1.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace  std;
 int main() {
-  
+    float personHeight, personWeight, personBMI;
+ cout<<"enter your height:";
+cin>>personHeight;
+cout<<"enter your weight:";
+cin>>personWeight;
+personBMI= (personWeight) / (personHeight*personHeight);
+cout<<"your personBMI is:"<<personBMI<<endl;
   return 0;
 }
