5f1c0fae7172ce3f9ae45473e623d5a46692c86c
yoditgirma <149189296+yoditgirma@users.noreply.github.com>
2023-11-15 10:34:42 +0300

    Update assignment 1.cpp

--- ETS1422_15_Yodit_Girma/assignment 1.cpp
+++ ETS1422_15_Yodit_Girma/assignment 1.cpp
@@ -10,3 +10,25 @@ personBMI= (personWeight) / (personHeight*personHeight);
 cout<<"your personBMI is:"<<personBMI<<endl;
   return 0;
 }
+
+#include <iostream>
+
+int main() {
+    double tankCapacity, milesPerGallon;
+
+    // Prompt the user for the capacity of the fuel tank
+    std::cout << "Enter the capacity of the fuel tank in gallons: ";
+    std::cin >> tankCapacity;
+
+    // Prompt the user for the miles per gallon the automobile can be driven
+    std::cout << "Enter the miles per gallon the automobile can be driven: ";
+    std::cin >> milesPerGallon;
+
+    // Calculate the number of miles the automobile can be driven without refueling
+    double milesWithoutRefueling = tankCapacity * milesPerGallon;
+
+    // Output the result
+    std::cout << "The automobile can be driven for " << milesWithoutRefueling << " miles without refueling." << std::endl;
+
+    return 0;
+}
