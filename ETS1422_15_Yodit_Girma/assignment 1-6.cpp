7e660d8202d7a63444ee47c18bc5587c80715385
yoditgirma <149189296+yoditgirma@users.noreply.github.com>
2023-11-17 21:10:02 +0300

    Update assignment 1-6.cpp

--- ETS1422_15_Yodit_Girma/assignment 1-6.cpp
+++ ETS1422_15_Yodit_Girma/assignment 1-6.cpp
@@ -1,3 +1,4 @@
+/*QUESTION NUMBER 1 FINDING BMI*/
 #include<iostream>
 using namespace  std;
 int main() {
@@ -10,3 +11,29 @@ personBMI= (personWeight) / (personHeight*personHeight);
 cout<<"your personBMI is:"<<personBMI<<endl;
   return 0;
 }
+
+/*QUESTION NUMBER 2 FINDING NUMBER OF MILES WITHOUT REFUELING*/
+#include <iostream>
+using namespace std;
+int main() {
+    double tankCapacity, milesPerGallon;
+
+    // Prompt the user for the capacity of the fuel tank
+    cout << "Enter the capacity of the fuel tank in gallons: ";
+    cin >> tankCapacity;
+
+    // Prompt the user for the miles per gallon the automobile can be driven
+   cout << "Enter the miles per gallon the automobile can be driven: ";
+   cin >> milesPerGallon;
+
+    // Calculate the number of miles the automobile can be driven without refueling
+    double milesWithoutRefueling = tankCapacity * milesPerGallon;
+
+    // Output the result
+   cout << "The automobile can be driven for " << milesWithoutRefueling << " miles without refueling." <<endl;
+
+    return 0;
+}
+
+/*QUESTION NUMBER 3 CONVERTING UPPER CASE TO LOWER CASE*/
+
