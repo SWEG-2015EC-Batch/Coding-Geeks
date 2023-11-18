87c6b6f8bd022584e803565efb5292b404d834eb
Yea65ray <149162964+Yea65ray@users.noreply.github.com>
2023-11-18 10:31:01 +0300

    Update Activity.cpp

--- ETS1399_15_Yeabsra_Yideg/Assignment.cpp
+++ ETS1399_15_Yeabsra_Yideg/Assignment.cpp
@@ -13,3 +13,24 @@ personBMI= (personWeight) / (personHeight*personHeight);
 cout<<"your personBMI is:"<<personBMI<<endl;
     return 0;
 }
+
+// Question 3
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+
+int main() {
+    char input;
+    cout << "Enter a letter: ";
+    cin >> input;
+
+    if (islower(input)) {
+        cout << "The uppercase equivalent is: " << (char)toupper(input) << endl;
+    } else if (isupper(input)) {
+        cout << "The lowercase equivalent is: " << (char)tolower(input) << endl;
+    } else {
+        cout << "Invalid input. Please enter a letter." << endl;
+    }
+
+    return 0;
+}
