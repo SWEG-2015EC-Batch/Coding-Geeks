06313cae650c1d38f81640e90de45580988434b8
Lordyonas <149390055+Lordyonas@users.noreply.github.com>
2023-11-18 11:30:22 +0300

    BMI.cpp

--- ETS1520_15_Yonas_Eneyew/BMI.cpp
+++ ETS1520_15_Yonas_Eneyew/BMI.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float Height, Weight, BMI;
+ cout<<"enter your height (in meter):";
+cin>>Height;
+cout<<"enter your weight (in kg):";
+cin>>Weight;
+BMI= (Weight) / (Height*Height);
+cout<<"your BMI is:"<<BMI;
 
+return 0;
+}
