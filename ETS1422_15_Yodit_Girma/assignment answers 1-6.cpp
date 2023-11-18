25cce9dd1bc39d6cab8ba619ceaeaea845718b39
yoditgirma <149189296+yoditgirma@users.noreply.github.com>
2023-11-18 10:23:11 +0300

    Update assignment answers 1-6.cpp

--- ETS1422_15_Yodit_Girma/assignment answers 1-6.cpp
+++ ETS1422_15_Yodit_Girma/assignment answers 1-6.cpp
@@ -76,4 +76,34 @@ int main()
 
 
 /*QUESTION NUMBBER 5 
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Input employee's information
+    string full_name;
+    float weekly_hours, bonus_rate, base_salary;
+    cout << "Please enter the employee's full name: ";
+    getline(cin, full_name);
+    cout << "Please enter the employee's weekly working_hours: ";
+    cin >> weekly_hours;
+    cout << "Please enter the employee's bonus _rate_per_hour: ";
+    cin >> bonus_rate;
+    cout << "Please enter the employee's base_salary: ";
+    cin >> base_salary;
+    // Calculate gross salary
+    float gross_salary = (weekly_hours * base_salary) + (bonus_rate * weekly_hours);
+  // Calculate deductions
+  float pension_deduction = 0.05 * gross_salary;
+  float tax_deduction = 0.15 * gross_salary;
+    // Calculate net salary
+   float net_salary = gross_salary - (pension_deduction + tax_deduction);
+     // Calculate bonus payment
+    float  bonus_payment = bonus_rate * weekly_hours;
+     cout << "Employee's Name: " << full_name << endl;
+    cout << "Gross_Salary: " << gross_salary << endl;
+    cout << "Net_Salary: " << net_salary << endl;
+    cout << "Bonus_Payment: " << bonus_payment << endl;
+    return 0;
+}
 
