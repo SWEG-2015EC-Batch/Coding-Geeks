2400ce32d921bee61d194e4f5fb6b73e6d842074
Truket Lema <149225623+truketlema@users.noreply.github.com>
2023-11-29 14:12:10 +0300

    Create V1.cpp

---
+++ ETS1318_15_Truket_Lema/V1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float person_Height, person_Weight, person_BMI;
+char gender;
+cout<<"enter your gender:";
+cin>>gender;
+ cout<<"enter your height:";
+cin>>person_Height;
+cout<<"enter your weight:";
+cin>>person_Weight;
+person_BMI= (person_Weight) / (person_Height*person_Height);
+cout<<"person gender:"<<gender<<endl;
+cout<<"your person_BMI is:"<<person_BMI;
+ return 0;
+}
