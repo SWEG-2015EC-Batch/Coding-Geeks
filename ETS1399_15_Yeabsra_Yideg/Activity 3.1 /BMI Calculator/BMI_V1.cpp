6439f40cf2fc8831173ddb707f12a61183f3b847
Yea65ray <149162964+Yea65ray@users.noreply.github.com>
2023-12-16 12:10:58 +0300

    Update BMI_V1.cpp

--- ETS1399_15_Yeabsra_Yideg/Activity 3.1 /BMI Calculator/BMI_V1.cpp
+++ ETS1399_15_Yeabsra_Yideg/Activity 3.1 /BMI Calculator/BMI_V1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float Height, Weight, BMI;
+cout<<"enter your height (in meter):";
+cin>>Height;
+cout<<"enter your weight (in kg):";
+cin>>Weight;
+cout<<"enter gender";
+cin>>gender;
 
+BMI= (Weight) / (Height*Height);
+
+cout<<"your BMI is:"<<BMI;  
+    
+    return 0;
+
+}
