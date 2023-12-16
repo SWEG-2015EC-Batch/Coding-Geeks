10e2e0a0214f66263ac4499c292527d8f7636eb8
yoditgirma <149189296+yoditgirma@users.noreply.github.com>
2023-12-16 08:46:54 -0800

    Update Grade_v1.cpp

--- ETS1422_15_Yodit_Girma/activity3.1/Grade_calculator/Grade_v1.cpp
+++ ETS1422_15_Yodit_Girma/activity3.1/Grade_calculator/Grade_v1.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    double mark;
+
+    cout << "Enter your mark that you get from the course: ";
+    cin >> mark;
+
+    if (mark >= 90) {
+        cout << "Your grade is: A+" << endl;
+    } else if (mark >= 80) {
+        cout << "Your grade is: A" << endl;
+    } else if (mark >= 75) {
+        cout << "Your grade is: B+" << endl;
+    } else if (mark >= 60) {
+        cout << "Your grade is: B" << endl;
+    } else if (mark >= 55) {
+        cout << "Your grade is: C+" << endl;
+    } else if (mark >= 45) {
+        cout << "Your grade is: C" << endl;
+    } else if (mark >= 30) {
+        cout << "Your grade is: D" << endl;
+    } else {
+        cout << "Your grade is: F" << endl;
+    }
+
+    return 0;
+}
