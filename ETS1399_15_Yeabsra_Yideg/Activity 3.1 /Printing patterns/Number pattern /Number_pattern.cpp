5c80feb7242421bf900a83d8d035f9487c1cbad0
Yea65ray <149162964+Yea65ray@users.noreply.github.com>
2023-12-17 20:11:26 +0300

    Create Number_pattern.cpp

---
+++ ETS1399_15_Yeabsra_Yideg/Activity 3.1 /Printing patterns/Number pattern /Number_pattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j;
+    int num,
+    bool stop = false;
+    
+    do{
+        cout<<"Enter a Number: ";
+        cin>>num;
+    cout << "The number of Pattern is look like: \n\n";
+
+    for ( i = 1; i <= num; i++) {
+        for ( j = 1; j <= num; j++) {
+            cout <<j<< " ";
+        }
+         
+        cout << endl;
+    }
+    cout<<"\nEnter 0 to exit or Enter 1 to continue: ";
+    cin>>stop;
+    }while(stop != 0);
+
+    return 0; 
+}
