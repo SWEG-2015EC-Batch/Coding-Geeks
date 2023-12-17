df111a822c07de29878a341aef3cfe5212e5db9e
Yea65ray <149162964+Yea65ray@users.noreply.github.com>
2023-12-17 20:09:48 +0300

    Update abcPattern.cpp

--- ETS1399_15_Yeabsra_Yideg/Activity 3.1 /Printing patterns/ABC pattern/abcPattern.cpp
+++ ETS1399_15_Yeabsra_Yideg/Activity 3.1 /Printing patterns/ABC pattern/abcPattern.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    char i, j;
+    
+    bool stop = false;
+    
+    do{
+    cout << "An Alphabets of Pattern is look like: \n\n";
+
+    for ( i = 'a'; i <= 'e'; i++) {
+        for ( j = 'a'; j <= 'e'; j++) {
+            cout <<j<< " ";
+        }
+         
+        cout << endl;
+    }
+    cout<<"\nEnter 0 to exit or Enter 1 to continue: ";
+    cin>>stop;
+    }while(stop != 0);
+
+    return 0; 
+}
