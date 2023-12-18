d3a21dfe9fde238f09a5bddf6298cb62d99e65e7
yoditgirma <149189296+yoditgirma@users.noreply.github.com>
2023-12-18 10:42:35 +0300

    Update Income_v2.cpp

--- ETS1422_15_Yodit_Girma/activity3.1/Income_calculator/Income_v2.cpp
+++ ETS1422_15_Yodit_Girma/activity3.1/Income_calculator/Income_v2.cpp
@@ -9,14 +9,27 @@ int main()
      cin>>Gross_salary;
      cout<<"Enter worked hour "<<endl;
      cin>>Workedhour;
-     cout<<"Enter income tax rate "<<endl;
-     cin>>Incometax;
+     if(Gross_salary>0 && Gross_salary<200){
+        Incometax==0;
+     }else if(Gross_salary>=200 && Gross_salary<600){
+        Incometax==0.1;
+     }else if(Gross_salary>=600 && Gross_salary<1200){
+        Incometax==0.15;
+     }else if(Gross_salary>=1200 && Gross_salary<2000){
+        Incometax==0.2;
+     }else if(Gross_salary>=2000 && Gross_salary<3500){
+        Incometax==0.25;
+     }else if(Gross_salary>=3500){
+        Incometax==0.3;
+     }else {
+        cout<<"error"<<endl;
+     }
 
         netsalary=Gross_salary-(Gross_salary*0.07)-(Gross_salary*Incometax);
-        cout<<"The net salary is "<< netsalary<<endl;
+        cout<<"the net salary is "<< netsalary<<endl;
 
     if(Workedhour>40) {
-    cout<<"Enter the overtime bonus hour "<<endl;
+    cout<<"Enter the overtime bonus hour"<<endl;
     cin>>Overtimehour;
     Bonuspayment=(Workedhour-40)*Overtimehour;
     }
@@ -25,3 +38,4 @@ int main()
 
      return 0;
 }
+
