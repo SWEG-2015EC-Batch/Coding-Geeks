537703ec47954e5ee3a0cf4fbe7b2b393482f027
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2023-12-18 10:33:30 +0300

    Update BMI.cpp

--- ETS1519_15_Ruth_Areda/ACTIVITY 3.1/BMI CALCULATOR/BMI.cpp
+++ ETS1519_15_Ruth_Areda/ACTIVITY 3.1/BMI CALCULATOR/BMI.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+	float H ,M ,BMI; 
+		cout<< "please enter the Height" <<endl;
+		cin>>H;
+			cout<< "please enter MASS "<<endl;
+		cin>>M;
+		BMI=M/(H*H) ;
+		cout<<"BMI "<<BMI; 
+		return 0;
+}
