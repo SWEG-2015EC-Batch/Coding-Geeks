92aba63fa1a3fe48e904fa37db60cf4d5e7c8772
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2023-12-18 19:21:53 +0300

    Update Playing with Digit.cpp

--- ETS1519_15_Ruth_Areda/ACTIVITY 3.1/Playing with Digit/Playing with Digit.cpp
+++ ETS1519_15_Ruth_Areda/ACTIVITY 3.1/Playing with Digit/Playing with Digit.cpp
@@ -55,6 +55,95 @@ int main() {
     return 0;
 }
 //Prints the first and the last digit of the number and find their sum.
+#include <iostream>
+using namespace std ;
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    int lastDigit = number % 10;
+    while (number >= 10) {
+        number /= 10;
+    }
+    int firstDigit = number;
+    int sum = firstDigit + lastDigit;
+    cout << "First digit: " << firstDigit <<endl;
+    cout << "Last digit: " << lastDigit <<endl;
+    cout << "Sum of the first and last digit: " << sum <<endl;
+
+    return 0;
+}
+//Swap the first and the last digit of the number.
+int swapFirstAndLastDigit(int num) {
+    int digits = countDigits(num);
+    if (digits <= 1) {
+        return num;
+    }
+
+    int lastDigit = num % 10;
+    int firstDigit = num / pow(10, digits - 1);
+    int middlePart = num % static_cast<int>(pow(10, digits - 1)) / 10;
+
+    return lastDigit * static_cast<int>(pow(10, digits - 1)) + middlePart * 10 + firstDigit;
+}
+//Check whether a number is palindrome or not.
+#include <iostream>
+
+bool isPalindrome(int number) {
+    int originalNumber = number;
+    int reversedNumber = 0;
+
+    while (number > 0) {
+        int lastDigit = number % 10;
+        reversedNumber = reversedNumber * 10 + lastDigit;
+        number /= 10;
+    }
+
+    return originalNumber == reversedNumber;
+}
+
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    if (isPalindrome(number)) {
+        std::cout << number << " is a palindrome number." << std::endl;
+    } else {
+        std::cout << number << " is not a palindrome number." << std::endl;
+    }
+
+    return 0;
+}
+//Find the frequency of each digit in a given integer and print in table format.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
