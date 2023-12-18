5caaac3d26e3c162a3e4881ad59509b91ddc0c66
Lordyonas <149390055+Lordyonas@users.noreply.github.com>
2023-12-18 10:34:42 -0800

    Update Playing_with_digit(g).cpp

--- ETS1520_15_Yonas_Eneyew/Activity 3.1/Playing_with_digit/Playing_with_digit(g).cpp
+++ ETS1520_15_Yonas_Eneyew/Activity 3.1/Playing_with_digit/Playing_with_digit(g).cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int num, reversedNum = 0, originalNum;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    originalNum = num;
+    while (num > 0) {
+        reversedNum = reversedNum * 10 + num % 10;
+        num /= 10;
+    }
+    if (originalNum == reversedNum) {
+        cout << "The number is a palindrome." << endl;
+    } else {
+        cout << "The number is not a palindrome." << endl;
+    }
+
+    return 0;
+}
