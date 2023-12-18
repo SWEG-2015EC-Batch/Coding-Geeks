adef24f91601fa726cc92b3d4c3e133e8f528ff9
Lordyonas <149390055+Lordyonas@users.noreply.github.com>
2023-12-18 10:33:50 -0800

    Update Playing_with_digit(e).cpp

--- ETS1520_15_Yonas_Eneyew/Activity 3.1/Playing_with_digit/Playing_with_digit(e).cpp
+++ ETS1520_15_Yonas_Eneyew/Activity 3.1/Playing_with_digit/Playing_with_digit(e).cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int num, firstDigit, lastDigit, sum;
+    cout << "Enter a number: ";
+    cin >> num;
+    lastDigit = num % 10;
+    while (num >= 10) {
+        num /= 10;
+    }
+    firstDigit = num;
+    sum = firstDigit + lastDigit;
+    cout << "First digit: " << firstDigit << endl;
+    cout << "Last digit: " << lastDigit << endl;
+    cout << "Sum of first and last digit: " << sum << endl;
+
+    return 0;
+}
