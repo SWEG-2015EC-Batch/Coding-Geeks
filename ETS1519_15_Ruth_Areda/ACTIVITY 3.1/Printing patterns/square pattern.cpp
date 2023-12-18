81abdb4330c336ff80730f39d4c2ece091dc900b
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2023-12-18 21:05:03 +0300

    Update square pattern.cpp

--- ETS1519_15_Ruth_Areda/ACTIVITY 3.1/Printing patterns/square pattern.cpp
+++ ETS1519_15_Ruth_Areda/ACTIVITY 3.1/Printing patterns/square pattern.cpp
@@ -1,1 +1,40 @@
+//full square
+#include <iostream>
+
+int main() {
+    int n;
+    std::cout << "Enter the number of rows for the full square: ";
+    std::cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+
+        for (int j = 1; j <= n; ++j) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
+//hollow square
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number of rows for the hollow square: ";
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (i == 1 || i == n || j == 1 || j == n) {
+                std::cout << "* ";
+            } else {
+                std::cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
 
