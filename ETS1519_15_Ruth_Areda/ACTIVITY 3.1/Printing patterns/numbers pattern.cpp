b02f689aa47411fd552416da5ff73dc1dc83cd57
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2023-12-18 21:06:59 +0300

    Update numbers pattern.cpp

--- ETS1519_15_Ruth_Areda/ACTIVITY 3.1/Printing patterns/numbers pattern.cpp
+++ ETS1519_15_Ruth_Areda/ACTIVITY 3.1/Printing patterns/numbers pattern.cpp
@@ -1,1 +1,21 @@
+//1to 5 square
+#include <iostream>
+using namespace std;
+int main() {
+    int rows = 5;
+    int columns = 5;
+    for (int i = 1; i <= rows; ++i) {
 
+        for (int j = 1; j <= columns; ++j) {
+        
+            cout << j;
+            if (j < columns) {
+                std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
+//
