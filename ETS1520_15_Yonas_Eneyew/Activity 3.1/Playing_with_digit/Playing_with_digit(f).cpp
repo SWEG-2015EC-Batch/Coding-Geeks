e95c3b2eac5fc93e4b41b091e5f9860daa9c5d88
Lordyonas <149390055+Lordyonas@users.noreply.github.com>
2023-12-18 10:34:11 -0800

    Update Playing_with_digit(f).cpp

--- ETS1520_15_Yonas_Eneyew/Activity 3.1/Playing_with_digit/Playing_with_digit(f).cpp
+++ ETS1520_15_Yonas_Eneyew/Activity 3.1/Playing_with_digit/Playing_with_digit(f).cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main() {
+    int num, firstDigit, lastDigit, digits = 0, temp;
+    cout << "Enter a number: ";
+    cin >> num;
+    lastDigit = num % 10;
+    temp = num;
+    while (temp > 0) {
+        digits++;
+        temp /= 10;
+    }
+    firstDigit = num / pow(10, digits-1);
+    num = num - (firstDigit * pow(10, digits-1)) - lastDigit + (lastDigit * pow(10, digits-1)) + firstDigit;
+    cout << "Number after swapping first and last digit: " << num << endl;
+
+    return 0;
+}
