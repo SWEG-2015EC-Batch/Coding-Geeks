5a2ac11716c835849c10ef11e302751ffe663889
Truket Lema <149225623+truketlema@users.noreply.github.com>
2023-12-18 22:16:26 +0300

    Create inverted_half_pyramid.cpp

---
+++ ETS1318_15_Truket_Lema/activity3.1/Printing patterns/pyramid pattern/inverted_half_pyramid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter the number of rows for the inverted pyramid: ";
+    cin >> rows;
+
+    for (int i = rows; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
