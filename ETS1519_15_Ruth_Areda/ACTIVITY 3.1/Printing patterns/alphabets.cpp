657972fe4bf480365a2df25bc791539520e90843
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2023-12-18 21:32:39 +0300

    Update alphabets.cpp

--- ETS1519_15_Ruth_Areda/ACTIVITY 3.1/Printing patterns/alphabets.cpp
+++ ETS1519_15_Ruth_Areda/ACTIVITY 3.1/Printing patterns/alphabets.cpp
@@ -20,7 +20,55 @@ int main() {
         }
         cout << endl;
     }
+//half
+    #include<iostream>
 
+using namespace std;
+int main(){
+    char i, j, rows;
+    bool stop = false;
+    do{
+    cout << "Enter an Alpha bet or a upto z latter of rows for the half pyramid: ";
+    cin >> rows;
+    
+    for(i ='a'; i <= rows; i++){
+        for(j = 'a'; j <= i; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"Entet 0 to exit Or enter 1 to countinue: ";
+    cin>>stop;
+    }while(stop != 0);
+    
+    return 0;
+}
+    // abc pattern
+    #include <iostream>
+using namespace std;
+
+int main() {
+    char i, j;
+    
+    bool stop = false;
+    
+    do{
+    cout << "An Alphabets of Pattern is look like: \n\n";
+
+    for ( i = 'a'; i <= 'e'; i++) {
+        for ( j = 'a'; j <= 'e'; j++) {
+            cout <<j<< " ";
+        }
+         
+        cout << endl;
+    }
+    cout<<"\nEnter 0 to exit or Enter 1 to continue: ";
+    cin>>stop;
+    }while(stop != 0);
+
+    return 0; 
+}
+    
     return 0;
 }
 
