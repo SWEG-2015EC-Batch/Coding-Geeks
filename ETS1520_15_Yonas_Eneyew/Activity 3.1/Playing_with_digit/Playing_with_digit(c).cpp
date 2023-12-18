182b31ff2b4c077dee2ea78dcbe93ae364f23d0b
Lordyonas <149390055+Lordyonas@users.noreply.github.com>
2023-12-18 10:33:01 -0800

    Update Playing_with_digit(c).cpp

--- ETS1520_15_Yonas_Eneyew/Activity 3.1/Playing_with_digit/Playing_with_digit(c).cpp
+++ ETS1520_15_Yonas_Eneyew/Activity 3.1/Playing_with_digit/Playing_with_digit(c).cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+
+   int sum = 0;
+int num;
+cout<<"enter any number:";
+    cin>>num;
+while(num>0)
+{
+    sum += num%10;
+    num /= 10;
+
+}
+cout<<sum;
+return 0;
+}
