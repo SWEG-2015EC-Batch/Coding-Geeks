338c05901e08e6c52777e9c3107274d8d00ab49a
Truket Lema <149225623+truketlema@users.noreply.github.com>
2023-12-18 21:33:35 +0300

    Create abc_pattern.cpp

---
+++ ETS1318_15_Truket_Lema/activity3.1/Printing patterns/abc_pattern/abc_pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows for the rectangular pattern: ";
+    cin >> rows;
+    cout << "Enter the number of columns for the rectangular pattern: ";
+    cin >> cols;
+
+    char letter = 'A';
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= cols; j++) {
+            cout << letter << " ";
+            letter++;
+            if (letter > 'Z') {
+                letter = 'A';
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
