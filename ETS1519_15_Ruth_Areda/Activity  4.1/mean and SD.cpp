3356b4166335ccf3a324d707f3e7feeff750d43f
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2023-12-19 20:45:40 +0300

    Update mean and SD.cpp

--- ETS1519_15_Ruth_Areda/Activity  4.1/mean and SD.cpp
+++ ETS1519_15_Ruth_Areda/Activity  4.1/mean and SD.cpp
@@ -1,1 +1,32 @@
+//Print the input numbers ,Compute and print the mean, and standard deviation
+#include <iostream>
+#include <cmath> 
+using namespace std;
+int main() {
+    int count = 0; 
+    double sum = 0; 
+    double sumOfSquares = 0; 
+    cout << "Enter a list of numbers. Enter a non-numeric value to stop.\n";
+    while (true) {
+        double num;
+        if (cin >> num) {
+            count++;
+            sum += num;
+            sumOfSquares += num * num;
+            cout << "You entered: " << num <<endl;
+        } else {
+            break;
+        }
+    }
+    if (count == 0) {
+        cout << "No numbers entered. Cannot calculate mean and standard deviation.\n";
+    } else {
+        double mean = sum / count;
+        double variance = (sumOfSquares / count) - (mean * mean);
+        double standardDeviation = sqrt(variance);
+        cout << "Mean: " << mean <<endl;
+        cout << "Standard Deviation: " << standardDeviation <<endl;
+    }
+    return 0;
+}
 
