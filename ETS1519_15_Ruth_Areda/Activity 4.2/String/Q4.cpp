d17a6eef1d00324b38ad65b161aac3713d41c3a5
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 21:03:09 +0300

    Update Q4.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/String/Q4.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/String/Q4.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main() {
+    std::string inputLine;
+    std::cout << "Enter a line of text: ";
+    std::getline(std::cin, inputLine);
+    std::istringstream stream(inputLine);
+    std::string token;
+
+    std::cout << "\nTokenized words:\n";
+    while (stream >> token) {
+        std::cout << token << "\n";
+    }
+
+    return 0;
+}
 
