193f89d77d513817e331c4facded39e4d0868577
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 17:24:48 +0300

    Update Q3.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/Array/2D Array/Q3.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/Array/2D Array/Q3.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int test, assignment, quiz, project, finalExam;
+
+  cout << "Enter test score: ";
+    cin >> test;
+   cout << "Enter assignment score: ";
+    cin >> assignment;
+    cout << "Enter quiz score: ";
+    cin >> quiz;
+    cout << "Enter project score: ";
+    cin >> project;
+    cout << "Enter final exam score: ";
+    cin >> finalExam;
+    int totalScore = test + assignment + quiz + project + finalExam;
+    string scoreLevel;
+    if (totalScore >= 80) {
+        scoreLevel = "Excellent";
+    } else if (totalScore >= 60) {
+        scoreLevel = "Very Good";
+    } else if (totalScore >= 50) {
+        scoreLevel = "Fair";
+    } else if (totalScore >= 40) {
+        scoreLevel = "Poor";
+    } else {
+        scoreLevel = "Fail";
+    }
+    cout << "\nResults:\n";
+    cout << "----------------------------------"<<endl;
+    cout << "Category       |   Score\n";
+    cout << "----------------------------------"<<endl;
+    cout << "Test           |   " << test <<endl;
+    cout << "Assignment     |   " << assignment <<endl;
+    cout << "Quiz           |   " << quiz <<endl;
+    cout << "Project        |   " << project  <<endl;
+    cout << "Final Exam     |   " << finalExam <<endl;
+    cout << "----------------------------------"<<endl;
+    cout << "Total Score    |   " << totalScore <<endl;
+    cout << "Score Level    |   " << scoreLevel <<endl;
+    cout << "----------------------------------"<<endl;
+
+    return 0;
+}
 
