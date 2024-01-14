317e79e44bf8a4eb17349e9b0016af5569a9c49d
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 21:58:26 +0300

    Update Q12.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/String/Q12.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/String/Q12.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    const int NUM_QUESTIONS = 10;
+    string correctAnswers = "ABCDABCDAB";  
+    string studentAnswers;
+    cout << "Enter your answers for the 10 questions (use uppercase letters A, B, C, or D):\n";
+    cin >> studentAnswers;
+    if (studentAnswers.length() != NUM_QUESTIONS) {
+        cerr << "Error: Invalid number of answers entered.\n";
+        return 1;  
+    }
+    int score = 0;
+    string markedAnswers;
+    for (int i = 0; i < NUM_QUESTIONS; ++i) {
+        if (studentAnswers[i] == correctAnswers[i]) {
+            score++;
+            markedAnswers += '+';
+        } else {
+            markedAnswers += '-';
+        }
+    }
+
+    cout << "\nTest Score: " << score << " out of " << NUM_QUESTIONS << "\n";
+    cout << "Marked Answers: " << markedAnswers << "\n";
+
+    return 0;
+}
 
