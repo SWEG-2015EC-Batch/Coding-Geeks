12dfd85f3b8b653af17eb20ac7be92ff8c7b61d8
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 20:41:23 +0300

    Update Q1.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/String/Q1.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/String/Q1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string inputString;
+    int vowelCount = 0, consonantCount = 0;
+   cout << "Enter a string: ";
+    getline(cin, inputString);
+    for (char ch : inputString) {
+        ch = tolower(ch);
+        if (isalpha(ch)) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                ++vowelCount;
+            } else {
+                ++consonantCount;
+            }
+        }
+    }
+    cout << "\nResults:\n";
+     cout << "---------------------------\n";
+    cout << "Input String: " << inputString << "\n";
+    cout << "Vowel Count: " << vowelCount << "\n";
+    cout << "Consonant Count: " << consonantCount << "\n";
+    cout << "---------------------------\n";
+
+    return 0;
+}
 
