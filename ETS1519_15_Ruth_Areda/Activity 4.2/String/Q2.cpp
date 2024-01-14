80d36b5673cce8bc49f99d4d16a296cf5047653e
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 20:50:31 +0300

    Update Q2.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/String/Q2.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/String/Q2.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string word;
+    cout << "Enter a word: ";
+    cin >> word;
+
+    int start = 0;
+    int end = word.length() - 1;
+    bool isPalindrome = true;
+
+    while (start <= end && isPalindrome) {
+        if (word[start++] != word[end--]) {
+            isPalindrome = false;
+        }
+    }
+
+    cout << "The word " << word << " is " << (isPalindrome ? "" : "not ") << "a palindrome.\n";
+
+    return 0;
+}
 
