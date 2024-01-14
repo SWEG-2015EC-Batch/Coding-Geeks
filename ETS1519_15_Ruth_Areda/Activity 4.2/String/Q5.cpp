13f06cb9cf798cef28b5895b2526b01e96ab99d8
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 21:12:11 +0300

    Update Q5.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/String/Q5.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/String/Q5.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+int main() {
+    string inputString;
+    cout << "Enter a string: ";
+    getline(cin, inputString);
+    int vowelCount = 0, consonantCount = 0, digitCount = 0, specialCharCount = 0;
+    for (char ch : inputString) {
+        if (isalpha(ch)) {
+            char lowercaseCh = tolower(ch);
+            if (lowercaseCh == 'a' || lowercaseCh == 'e' || lowercaseCh == 'i' || lowercaseCh == 'o' || lowercaseCh == 'u') {
+                ++vowelCount;
+            } else {
+                ++consonantCount;
+            }
+        } else if (isdigit(ch)) {
+            ++digitCount;
+        } else if (ispunct(ch) || isspace(ch)) {
+            ++specialCharCount;
+        }
+    }
+    cout << "\nFrequency Analysis:\n";
+    cout << "Vowel Count: " << vowelCount << "\n";
+    cout << "Consonant Count: " << consonantCount << "\n";
+    cout << "Digit Count: " << digitCount << "\n";
+    cout << "Special Character Count: " << specialCharCount << "\n";
+  
+
+    return 0;
+}
 
