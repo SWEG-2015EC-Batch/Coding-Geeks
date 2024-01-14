a3d17e4dee1c933007d0c723121adb1534ede61a
yoditgirma <149189296+yoditgirma@users.noreply.github.com>
2024-01-14 13:57:54 +0300

    Update Q2.cpp

--- ETS1422_15_Yodit_Girma/Activity 4.2/String/Q2.cpp
+++ ETS1422_15_Yodit_Girma/Activity 4.2/String/Q2.cpp
@@ -1,1 +1,29 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string word;
+    cout << "Enter a word: ";
+    cin >> word;
+    int start, end;
+    start = 0;
+    end = word.length() - 1;
+    bool isPalindrome;
+    isPalindrome = true;
+    while (start <= end) {
+        if (word[start] != word[end]) {
+            isPalindrome = false;
+            break;
+        }
+        start++;
+        end--;
+    }
+    cout << "The word " << word << " ";
+    if (isPalindrome) {
+        cout << "is a palindrome.\n";
+    }
+    else {
+        cout << "is not a palindrome.\n";
+    }
+    return 0;
+}
