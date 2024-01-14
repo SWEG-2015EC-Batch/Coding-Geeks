c841b8ac182a57cdd8de508afb8ecddb1e9b9a78
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 21:38:57 +0300

    Update Q10.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/String/Q10.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/String/Q10.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    const int MAX_PASSWORD_LENGTH = 20;
+    char pass[MAX_PASSWORD_LENGTH];
+    string storedPassword = "securepassword";
+    strncpy(pass, storedPassword.c_str(), MAX_PASSWORD_LENGTH);
+    cout << "Enter the password: ";
+    string userInput;
+    getline(cin, userInput);
+    if (userInput == storedPassword) {
+        cout << "Password is correct. Access granted.\n";
+    } else {
+        cout << "Incorrect password. Access denied.\n";
+    }
+
+    return 0;
+}
 
