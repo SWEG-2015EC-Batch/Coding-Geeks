6ee2406ecb691a90160ba66aab677bb21f130f71
yoditgirma <149189296+yoditgirma@users.noreply.github.com>
2024-01-14 11:19:13 +0300

    Update Q7.cpp

--- ETS1422_15_Yodit_Girma/Activity 4.2/Array/1D/Q7.cpp
+++ ETS1422_15_Yodit_Girma/Activity 4.2/Array/1D/Q7.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int size;
+    cout << "Enter the size of the array: " << endl;
+    cin >> size;
+    int numbers[size];
+    cout << "Enter the numbers: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> numbers[i];
+    }
+    cout << "The even numbers in the array in reverse order are: " << endl;
+    for (int i = size - 1; i >= 0; i--)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            cout << numbers[i] << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
