bd0e6b665812bcbcacb76c1420b0551fae65fe1c
Yea65ray <149162964+Yea65ray@users.noreply.github.com>
2024-01-14 20:04:54 +0300

    Update Questionn_11.cpp

--- ETS1399_15_Yeabsra_Yideg/Activity_4.2/String/Questionn_11.cpp
+++ ETS1399_15_Yeabsra_Yideg/Activity_4.2/String/Questionn_11.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+  string first, middle, last;
+  cout << "Enter your first name: ";
+  cin >> first;
+  cout << "Enter your middle name: ";
+  cin >> middle;
+  cout << "Enter your last name: ";
+  cin >> last;
+  string initials = " ";
+  initials += first[0];
+  initials += middle[0];
+  initials += last[0];
+
+  cout << "Your initials are: " <<initials << "\n";
+
+  return 0; } 
