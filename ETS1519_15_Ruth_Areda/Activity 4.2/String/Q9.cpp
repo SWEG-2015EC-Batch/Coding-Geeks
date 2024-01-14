549046067bbfb19881270190ec1858c5bc570b1d
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 21:34:21 +0300

    Update Q9.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/String/Q9.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/String/Q9.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string favoriteProgram1 = "Breaking Bad";
+    string favoriteProgram2;
+    favoriteProgram2 = "Stranger Things";
+    cout << "My Two Favorite Television Programs:\n";
+    cout << "1. " << favoriteProgram1 << "\n";
+    cout << "2. " << favoriteProgram2 << "\n";
+
+    return 0;
+}
 
