549d01d2f66b946f2f94ae9a66dce18d6c883da0
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 21:30:04 +0300

    Update Q8.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/String/Q8.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/String/Q8.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string inputText;
+    cout << "Enter a line of text: ";
+    getline(cin, inputText);
+    cout << "\nText in Uppercase: ";
+    for (char& c : inputText) {
+        cout << static_cast<char>(toupper(c));
+    }
+    cout << "\nText in Lowercase: ";
+    for (char& c : inputText) {
+        cout << static_cast<char>(tolower(c));
+    }
+
+    cout << "\n";
+
+    return 0;
+}
 
