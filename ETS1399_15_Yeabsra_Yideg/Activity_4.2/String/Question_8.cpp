32544728da1732303db024d55e59b1f755ac7f5c
Yea65ray <149162964+Yea65ray@users.noreply.github.com>
2024-01-14 20:01:44 +0300

    Update Question_8.cpp

--- ETS1399_15_Yeabsra_Yideg/Activity_4.2/String/Question_8.cpp
+++ ETS1399_15_Yeabsra_Yideg/Activity_4.2/String/Question_8.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string text; 
+  cout << "Enter a line of text: ";
+  getline(cin, text);
+  string upper = text; 
+  string lower = text; 
+  for (int i = 0; i < upper.length(); i++) {
+    upper[i] = toupper(upper[i]);
+  }
+  for (int i = 0; i < lower.length(); i++) {
+    lower[i] = tolower(lower[i]);
+  }
+  cout << "The text in uppercase is: " << upper << "\n";
+  cout << "The text in lowercase is: " << lower << "\n";
+
+  return 0;
+}
 
