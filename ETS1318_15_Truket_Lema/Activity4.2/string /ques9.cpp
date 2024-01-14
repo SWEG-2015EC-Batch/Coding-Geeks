35d1db4857a076eb4091eb2aec31aac3c7b3e7ed
Truket Lema <149225623+truketlema@users.noreply.github.com>
2024-01-14 21:01:45 +0300

    Update ques9.cpp

--- ETS1318_15_Truket_Lema/Activity4.2/string /ques9.cpp
+++ ETS1318_15_Truket_Lema/Activity4.2/string /ques9.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+int main() {
+  char tv1[20] = "Breaking Bad";
+  char tv2[20];
+  strcpy(tv2, "Behind her eyes");
+  cout << "My two favorite television programs are: \n";
+  cout << tv1 << "\n";
+  cout << tv2 << "\n";
+  return 0;
+}
