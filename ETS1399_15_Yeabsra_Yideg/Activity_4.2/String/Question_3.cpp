5b730494de5ed47ae4604789a6f5cec2f7e63644
Yea65ray <149162964+Yea65ray@users.noreply.github.com>
2024-01-14 18:38:22 +0300

    Update Question_3.cpp

--- ETS1399_15_Yeabsra_Yideg/Activity_4.2/String/Question_3.cpp
+++ ETS1399_15_Yeabsra_Yideg/Activity_4.2/String/Question_3.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string input;
+    string output = "";
+    cout << "Enter a string: ";
+    getline(cin, input);
+    for (char c : input) {
+        if (isalpha(c)) {
+            output += c;
+        }
+    }
+    cout << "The output string is: " << output << "\n";
+    return 0;
+}   
