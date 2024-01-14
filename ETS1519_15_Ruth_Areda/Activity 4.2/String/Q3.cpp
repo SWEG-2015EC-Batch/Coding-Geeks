d31492e884a9a841d1206d85bed148b55aac564b
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 20:54:08 +0300

    Update Q3.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/String/Q3.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/String/Q3.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+int main() {
+    string inputString;
+    cout << "Enter a string: ";
+    getline(cin, inputString);
+    string resultString;
+    for (char ch : inputString) {
+        if (isalpha(ch)) {
+            resultString += ch;
+        }
+    }
+    cout << "\nOriginal String: " << inputString << "\n";
+    cout << "String with non-alphabet characters removed: " << resultString << "\n";
+
+    return 0;
+}
 
