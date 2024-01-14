4da3fbc00b23ec11f67ef128bda6da793f53001e
Ruthareda <149236910+Ruthareda@users.noreply.github.com>
2024-01-14 21:24:31 +0300

    Update Q7.cpp

--- ETS1519_15_Ruth_Areda/Activity 4.2/String/Q7.cpp
+++ ETS1519_15_Ruth_Areda/Activity 4.2/String/Q7.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Enter the number of names: ";
+  cin >> n;
+
+  string names[n];
+  string name;
+  cout << "Enter the names: \n";
+  for (int i = 0; i < n; i++) {
+    cin >> name;
+    names[i] = name;
+  }
+
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (names[j] > names[j + 1]) {
+        swap(names[j], names[j + 1]);
+      }
+    }
+  }
+
+  cout << "The names in alphabetical order are: \n";
+  for (int i = 0; i < n; i++) {
+    cout << names[i] << "\n";
+  }
+
+  return 0;
+}
 
